4cd47ce2e9b1f465353527e1d5806972b9908c61
Andressa Santos <henkeandressa@gmail.com>
2025-07-08 19:21:29 -0300

    feat: add rooms listing

---
+++ src/db/seed.ts
@@ -0,0 +1,19 @@
+import { reset, seed } from 'drizzle-seed'
+import { db, sql } from './connection.ts'
+import { schema } from './schema/index.ts'
+
+await reset(db, schema )
+
+await seed(db, schema ).refine((f) => {
+  return {
+    rooms: {
+      count: 20,
+      columns: {
+        name: f.fullName(),
+        description: f.loremIpsum()
+      },
+    },
+  }
+})
+
+await sql.end()
