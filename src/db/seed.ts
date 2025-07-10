e2301f32fd59a55f008dc562851007f0e5e278b1
Andressa Santos <henkeandressa@gmail.com>
2025-07-10 10:19:45 -0300

    feat: add room questions

--- src/db/seed.ts
+++ src/db/seed.ts
@@ -2,15 +2,18 @@ import { reset, seed } from 'drizzle-seed'
 import { db, sql } from './connection.ts'
 import { schema } from './schema/index.ts'
 
-await reset(db, schema )
+await reset(db, schema)
 
-await seed(db, schema ).refine((f) => {
+await seed(db, schema).refine((f) => {
   return {
     rooms: {
       count: 20,
       columns: {
         name: f.fullName(),
-        description: f.loremIpsum()
+        description: f.loremIpsum(),
+      },
+      with: {
+        questions: 5,
       },
     },
   }
