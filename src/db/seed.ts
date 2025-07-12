eb409f1e28da7f7d31960862e4399a669a38f71f
Andressa Santos <henkeandressa@gmail.com>
2025-07-12 00:15:53 -0300

    feat: gemini api integration and vector search

--- src/db/seed.ts
+++ src/db/seed.ts
@@ -7,7 +7,7 @@ await reset(db, schema)
 await seed(db, schema).refine((f) => {
   return {
     rooms: {
-      count: 20,
+      count: 25,
       columns: {
         name: f.fullName(),
         description: f.loremIpsum(),
